9c025aa4a0a496d616eb16ee4410b89f2be719bc
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-02-08 22:54:15 -0600

    Update index.jsx
    Included a button clicking mechanic to the add/edit section

--- SD/app/index.jsx
+++ SD/app/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, StyleSheet, SafeAreaView } from "react-native"
+import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native"
 
 const Index = () => {
   return (
@@ -15,9 +15,9 @@ const Index = () => {
         <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       </View>
 
-      <View style={styles.addDaySection}>
+      <TouchableOpacity style={styles.addDaySection}>
         <Text style={{textAlign: 'center'}}>[Add to/Edit Day Section]</Text>
-      </View>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
