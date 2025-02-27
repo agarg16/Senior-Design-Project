e81fb31f6a02f1c9c1dbda6ec923d4c9d0fae577
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-02-27 12:32:13 -0600

    Update index.tsx
    Modified today button to match left/right arrow color and gave shadow outline to "Today" text to increase visibility
    
    Modified selected day highlight to be a circle
    
    Updated today button to go to current date if on a year that is different from the current year
    
    Keyboard hides when tapping the screen (with the exception of left/right arrows and today button)
    
    Journal entry and edit button now display above the keyboard when typing in ios

--- SD/app/index.tsx
+++ SD/app/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { Text, View, StyleSheet, TextInput, Button, SafeAreaView, FlatList, Dimensions, Platform, TouchableOpacity, Image } from "react-native"
+import { Text, View, ScrollView, StyleSheet, TextInput, SafeAreaView, FlatList, Dimensions, Platform, TouchableOpacity, Image, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView } from "react-native"
 import { getMonthName } from '../additionalFiles/getMonthName.js' // Returns the name of the current month being considered
 import db from '../database/database';
-import { insertJournalEntry, getJournalEntries, setJournalEntry} from '../database/database';
+import { insertJournalEntry, getJournalEntries, setJournalEntry } from '../database/database';
 
 const calendarFlexSize = 4 // The flex size for the portion of the tab dedicated to the calendar
 
@@ -11,66 +11,65 @@ let height = 0 // Height of screen
 let daysBoxHeight = 0 // Height of the boxes holding the days of the current month
 
 // Determines the width and height of the screen and sets the boxes holding the days of the calendar month to a size
-if(Platform.OS === 'web') {
+if (Platform.OS === 'web') {
   width = Dimensions.get('window').width
   height = Dimensions.get('window').height
 
-  daysBoxHeight = width <= height? width / 30 : height / 30
+  daysBoxHeight = width <= height ? width / 30 : height / 30
 }
 else {
   width = Dimensions.get('screen').width
   height = Dimensions.get('screen').height
 
-  daysBoxHeight = width <= height? (width * 0.0725) - (width * 0.005): (height * 0.035) - (height * 0.0001)
+  daysBoxHeight = width <= height ? (width * 0.0725) - (width * 0.005) : (height * 0.035) - (height * 0.0001)
 }
 
 const curDate = new Date() // Currently selected date to be displayed (defaults to actual date)
 
-
 // All of the potential boxes that could be displayed on the calendar (6 possible weeks of 7 days = 42)
 const DAYS_BOXES = [
-  {id: 1},
-  {id: 2},
-  {id: 3},
-  {id: 4},
-  {id: 5},
-  {id: 6},
-  {id: 7},
-  {id: 8},
-  {id: 9},
-  {id: 10},
-  {id: 11},
-  {id: 12},
-  {id: 13},
-  {id: 14},
-  {id: 15},
-  {id: 16},
-  {id: 17},
-  {id: 18},
-  {id: 19},
-  {id: 20},
-  {id: 21},
-  {id: 22},
-  {id: 23},
-  {id: 24},
-  {id: 25},
-  {id: 26},
-  {id: 27},
-  {id: 28},
-  {id: 29},
-  {id: 30},
-  {id: 31},
-  {id: 32},
-  {id: 33},
-  {id: 34},
-  {id: 35},
-  {id: 36},
-  {id: 37},
-  {id: 38},
-  {id: 39},
-  {id: 40},
-  {id: 41},
-  {id: 42},
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
+  { id: 9 },
+  { id: 10 },
+  { id: 11 },
+  { id: 12 },
+  { id: 13 },
+  { id: 14 },
+  { id: 15 },
+  { id: 16 },
+  { id: 17 },
+  { id: 18 },
+  { id: 19 },
+  { id: 20 },
+  { id: 21 },
+  { id: 22 },
+  { id: 23 },
+  { id: 24 },
+  { id: 25 },
+  { id: 26 },
+  { id: 27 },
+  { id: 28 },
+  { id: 29 },
+  { id: 30 },
+  { id: 31 },
+  { id: 32 },
+  { id: 33 },
+  { id: 34 },
+  { id: 35 },
+  { id: 36 },
+  { id: 37 },
+  { id: 38 },
+  { id: 39 },
+  { id: 40 },
+  { id: 41 },
+  { id: 42 },
 ]
 
 
@@ -84,11 +83,11 @@ const Index = () => {
   const updateCalendarHeader = (directionChanged: number) => {
     setCurMonth((curMonth + directionChanged) % 12)
 
-    if(curMonth < 0) {
+    if (curMonth < 0) {
       setCurMonth(11)
       setCurYear(curYear - 1)
     }
-    else if(curMonth > 11) {
+    else if (curMonth > 11) {
       setCurMonth(0)
       setCurYear(curYear + 1)
     }
@@ -128,7 +127,7 @@ const Index = () => {
     setEntries(allEntries);
 
     const selectedEntry = entries.find(e => e.date === date);
-    setEntry(selectedEntry ? selectedEntry.entry : ""); 
+    setEntry(selectedEntry ? selectedEntry.entry : "");
   };
 
   useEffect(() => {
@@ -137,114 +136,125 @@ const Index = () => {
 
 
   return (
-    <SafeAreaView style={styles.container}>
-      {/* Calendar */}
-      <View style={{flex: calendarFlexSize, paddingLeft: '5%', paddingRight: '5%', marginBottom: '5%', justifyContent: 'flex-start'}}>
-        {/* Displays the month name and year above the calendar */}
-        <View style={{alignItems: 'center', justifyContent: 'center', flexDirection: 'row'}}>
-          {/* Left Arrow Button */}
-          <TouchableOpacity style={styles.arrowButtonsLocation} onPress={() => {
-            curDate.setMonth(curDate.getMonth() - 1)
-            {updateCalendarHeader(-1)}
-            setCurStartingDayOfWeek(new Date(curDate.getFullYear(), curDate.getMonth(), 1).getDay())
-            setNumDaysInCurMonth(new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0).getDate())
-          }}>
-            <Image style={styles.arrowButtonsImgs} source={require('../assets/images/leftArrow.png')} accessibilityLabel='Left Arrow' />
-          </TouchableOpacity>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : undefined} style={{flex: 1}}>
+          {/* Calendar */}
+          <View style={{ flex: calendarFlexSize, paddingLeft: '5%', paddingRight: '5%', marginBottom: '5%', justifyContent: 'flex-start' }}>
+            {/* Displays the month name and year above the calendar */}
+            <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}>
+              {/* Left Arrow Button */}
+              <TouchableOpacity style={styles.arrowButtonsLocation} onPress={() => {
+                curDate.setMonth(curDate.getMonth() - 1)
+                { updateCalendarHeader(-1) }
+                setCurStartingDayOfWeek(new Date(curDate.getFullYear(), curDate.getMonth(), 1).getDay())
+                setNumDaysInCurMonth(new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0).getDate())
+              }}>
+                <Image style={styles.arrowButtonsImgs} source={require('../assets/images/leftArrow.png')} accessibilityLabel='Left Arrow' />
+              </TouchableOpacity>
 
-          {/* Calendar Header (of Month and Year) */}
-          <Text style={{fontSize: 38, textAlign: 'center', width: 280}}>{getMonthName(curDate.getMonth())} {curDate.getFullYear()}</Text>
-        
-          {/* Right Arrow Button */}
-          <TouchableOpacity style={styles.arrowButtonsLocation} onPress={() => {
-            curDate.setMonth(curDate.getMonth() + 1)
-            {updateCalendarHeader(1)}
-            setCurStartingDayOfWeek(new Date(curDate.getFullYear(), curDate.getMonth(), 1).getDay())
-            setNumDaysInCurMonth(new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0).getDate())
-          }}>
-            <Image style={styles.arrowButtonsImgs} source={require('../assets/images/rightArrow.png')} />
-          </TouchableOpacity>
-        </View>
+              {/* Calendar Header (of Month and Year) */}
+              <Text style={{ fontSize: 38, textAlign: 'center', width: 280 }}>{getMonthName(curDate.getMonth())} {curDate.getFullYear()}</Text>
+
+              {/* Right Arrow Button */}
+              <TouchableOpacity style={styles.arrowButtonsLocation} onPress={() => {
+                curDate.setMonth(curDate.getMonth() + 1)
+                { updateCalendarHeader(1) }
+                setCurStartingDayOfWeek(new Date(curDate.getFullYear(), curDate.getMonth(), 1).getDay())
+                setNumDaysInCurMonth(new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0).getDate())
+              }}>
+                <Image style={styles.arrowButtonsImgs} source={require('../assets/images/rightArrow.png')} />
+              </TouchableOpacity>
+            </View>
 
 
-        {/* Displays the days of the week and days in the calendar */}
-        <View>
-          {/* Displays the days of the week at the top of the calendar */}
-          <View style={styles.calendarWeeksContainer}>
-            <Text style={styles.calendarDaysOfWeek}>Sun</Text>
-            <Text style={styles.calendarDaysOfWeek}>Mon</Text>
-            <Text style={styles.calendarDaysOfWeek}>Tues</Text>
-            <Text style={styles.calendarDaysOfWeek}>Wed</Text>
-            <Text style={styles.calendarDaysOfWeek}>Thurs</Text>
-            <Text style={styles.calendarDaysOfWeek}>Fri</Text>
-            <Text style={styles.calendarDaysOfWeek}>Sat</Text>
+            {/* Displays the days of the week and days in the calendar */}
+            <View>
+              {/* Displays the days of the week at the top of the calendar */}
+              <View style={styles.calendarWeeksContainer}>
+                <Text style={styles.calendarDaysOfWeek}>Sun</Text>
+                <Text style={styles.calendarDaysOfWeek}>Mon</Text>
+                <Text style={styles.calendarDaysOfWeek}>Tues</Text>
+                <Text style={styles.calendarDaysOfWeek}>Wed</Text>
+                <Text style={styles.calendarDaysOfWeek}>Thurs</Text>
+                <Text style={styles.calendarDaysOfWeek}>Fri</Text>
+                <Text style={styles.calendarDaysOfWeek}>Sat</Text>
+              </View>
+
+
+              {/* Displays the minimum weeks-worth of days needed to display all days of the month and a filler number for text in each day */}
+              <FlatList
+                data={DAYS_BOXES.slice(0, Math.ceil((numDaysInCurMonth + curStartingDayOfWeek) / 7) * 7)} // Ensures only the necesary weeks are displayed (entire blank weeks prevented)
+                renderItem={({ index }) => {
+                  const day = index - curStartingDayOfWeek + 1;
+                  const isSelected = curDate.getDate() === day;
+                  return (
+                    index >= curStartingDayOfWeek ?
+                      index < (numDaysInCurMonth + curStartingDayOfWeek) ?
+                        <TouchableOpacity
+                          style={styles.calendarDayOutlines}
+                          onPress={() => {
+                            curDate.setDate((index - curStartingDayOfWeek) + 1)
+                            { setCurDay((index - curStartingDayOfWeek) + 1) }
+                          }}>
+                          <Text style={[styles.calendarDayText, isSelected && styles.selectedDay]}>{(index - curStartingDayOfWeek) + 1}</Text>
+                        </TouchableOpacity>
+                        : <View style={styles.calendarDayOutlines}><Text></Text></View>
+                      : <View style={styles.calendarDayOutlines}><Text></Text></View>
+                  )
+                }}
+                scrollEnabled={false}
+                numColumns={7}
+                contentContainerStyle={{}}
+              />
+            </View>
+
+            {/* "Today" Button */}
+            <TouchableOpacity
+              style={styles.todayButton}
+              onPress={() => {
+                const today = new Date()
+                curDate.setFullYear(today.getFullYear(), today.getMonth(), today.getDate())
+                setCurDay(today.getDate())
+                setCurMonth(today.getMonth())
+                setCurYear(today.getFullYear())
+                setCurStartingDayOfWeek(new Date(curDate.getFullYear(), curDate.getMonth(), 1).getDay())
+                setNumDaysInCurMonth(new Date(curDate.getFullYear(), curDate.getMonth() + 1, 0).getDate())
+              }}>
+              <Text style={styles.todayButtonText}>Today</Text>
+            </TouchableOpacity>
           </View>
 
-          
-          {/* Displays 6 weeks-worth of days and a filler number for text in each day */}
-          <FlatList
-            data={DAYS_BOXES.slice(0, Math.ceil((numDaysInCurMonth + curStartingDayOfWeek) / 7) * 7)} // Ensures only the necesary weeks are displayed (entire blank weeks prevented)
-            renderItem={({index}) => {
-              const day = index - curStartingDayOfWeek + 1;
-              const isSelected = curDate.getDate() === day;
-              return(
-                index >= curStartingDayOfWeek ? 
-                  index < (numDaysInCurMonth + curStartingDayOfWeek) ? 
-                    <TouchableOpacity
-                    style={[styles.calendarDayOutlines, isSelected && styles.selectedDay]}
-                    onPress={() => {
-                      curDate.setDate((index - curStartingDayOfWeek) + 1)
-                      {setCurDay((index - curStartingDayOfWeek) + 1)}
-                    }}>
-                      <Text style={styles.calendarDayText}>{(index - curStartingDayOfWeek) + 1}</Text>
-                    </TouchableOpacity>
-                  : <View style={styles.calendarDayOutlines}><Text></Text></View>
-                : <View style={styles.calendarDayOutlines}><Text></Text></View>
-              )
-            }}
-            scrollEnabled={false}
-            numColumns={7}
-            contentContainerStyle={{}}
-          />
-        </View>
 
-      {/* "Today" Button */}
-      <TouchableOpacity
-        style={styles.todayButton}
-        onPress={() => {
-          const today = new Date();
-          curDate.setFullYear(today.getFullYear(), today.getMonth(), today.getDate());
-          setCurDay(today.getDate());
-        }}>
-        <Text style={styles.todayButtonText}>Today</Text>
-      </TouchableOpacity>
-    </View>
+          {/* Summary */}
+          <View style={{ backgroundColor: 'azure', padding: 15, borderWidth: 1, marginBottom: 8 }}>
+            {/* Current Date and Whether or Not There Are Journal Entries */}
+            <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10 }}>
+              {curDate.toLocaleString('default', { month: 'long' })} {curDate.getDate()}, {curDate.getFullYear()}
+            </Text>
+            <Text style={{marginBottom: 5}}>{selectedEntry ? selectedEntry.entry : "No journal entry for this day."}</Text>
 
+            {/* Entry Input */}
+            <ScrollView style={styles.journalInput} automaticallyAdjustKeyboardInsets={false}>
+              <TextInput
+                style={{padding: 8, height: 120}}
+                multiline
+                placeholder={entry ? entry : "Write your journal entry here..."}
+                placeholderTextColor={entry ? "black" : "gray"}
+                value={entry}
+                onChangeText={setEntry}
+              />
+            </ScrollView>
+          </View>
 
-      {/* Summary */}
-      <View style={{ padding: 15, borderWidth: 1, marginBottom: 10 }}>
-        <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-          {curDate.toLocaleString('default', { month: 'long' })} {curDate.getDate()}, {curDate.getFullYear()}
-        </Text>
-        <Text>{selectedEntry ? selectedEntry.entry : "No journal entry for this day."}</Text>
-
-        {/* Entry Input */}
-        <TextInput
-          style={styles.journalInput}
-          multiline
-          placeholder={entry ? entry : "Write your journal entry here..."}
-          placeholderTextColor={entry ? "black" : "gray"}
-          value={entry}
-          onChangeText={setEntry}
-        />
-        </View>
-
-        {/* Save Entry Button */}
-        <TouchableOpacity style={{ padding: 10, backgroundColor: "#ddd" }} onPress={handleAddEntry}>
-          <Text style={{ textAlign: "center" }}>[Edit Entry]</Text>
-        </TouchableOpacity>
+          {/* Save Entry Button */}
+          <TouchableOpacity style={{ padding: 10, backgroundColor: "#ddd", borderWidth: 0.25 }} onPress={handleAddEntry}>
+            <Text style={{ textAlign: "center" }}>[Edit Entry]</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
       </SafeAreaView>
-      );
+    </TouchableWithoutFeedback>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -286,24 +296,30 @@ const styles = StyleSheet.create({
     flexDirection: 'row'
   },
   calendarDayText: {
-    textAlign: 'center'
+    textAlign: 'center',
+    width: 25,
+    height: 25,
+    paddingTop: 4.15
   },
-    selectedDay: {
+  selectedDay: {
     backgroundColor: '#add8e6', // Light blue background to highlight selected day
-    borderRadius: 10, 
+    borderRadius: 50
   },
   todayButton: {
     marginTop: 10,
     padding: 10,
-    backgroundColor: '#007AFF',
+    backgroundColor: '#b1d8ff',
     alignItems: 'center',
     borderRadius: 5,
+    borderWidth: 0.5
   },
-
   todayButtonText: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 16,
+    fontSize: 18,
+    textShadowColor: 'black',
+    textShadowOffset: { width: 0.5, height: 0.5 },
+    textShadowRadius: 2
   },
   modifyDaySection: {
     backgroundColor: 'lightblue',
@@ -323,11 +339,10 @@ const styles = StyleSheet.create({
     borderColor: '#aaa',
     borderWidth: 1,
     borderRadius: 5,
-    padding: 10,
     fontSize: 16,
     backgroundColor: 'white',
-    textAlignVertical: 'top',
-  },
+    textAlignVertical: 'top'
+  }
 })
 
 export default Index
