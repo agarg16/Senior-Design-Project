9bcbbf6a5a53551ea522ffb41a3056e56743c1e5
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-11 23:44:21 -0500

    Update checkIn.tsx
    - Updated to include arrows to navigate between activity days and allow the user to go back to the current day by pressing the date

--- SD/app/checkIn.tsx
+++ SD/app/checkIn.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, SafeAreaView, FlatList, TouchableOpacity, Modal, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, Platform } from "react-native";
+import { Text, View, SafeAreaView, FlatList, TouchableOpacity, Modal, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, Platform, Image } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { modalStyles, styles } from '../styles/CheckInStyles';
 import { ActivityButton } from '../components/CheckInComponents/ActivityButton';
 import { UpdateModal } from '../components/CheckInComponents/UpdateModal';
 import { CreateModal } from '../components/CheckInComponents/CreateModal';
 import { MoodGrid } from '../components/CheckInComponents/MoodGrid';
+import { getMonthName } from '../additionalFiles/getMonthName';
 
 import { 
   addDate,
@@ -19,8 +20,8 @@ import {
 } from '../database/database';
 
 const CheckIn = () => {
-  /* var textBox1 = ""
-  var textBox2 = "" */
+  var currentDay = new Date()
+  currentDay = new Date(currentDay.getFullYear(), currentDay.getMonth(), currentDay.getDate())  /* Prevents timezone issue from displaying one date but doing calculations with another */
   const [modalType, setModalType] = useState("update")
   const [listOfActivityButtons, setListOfActivityButtons] = useState<string[]>([])
   const [updateTextWith, setUpdateTextWith] = useState("")
@@ -32,37 +33,39 @@ const CheckIn = () => {
   const [selectedActivityText, setSelectedActivityText] = useState("")
   const [selectedActivityVal, setSelectedActivityVal] = useState(0)
   const [curDate, setCurDate] = useState(new Date().toISOString().split("T")[0])
+  const [updateCurDateWith, setUpdateCurDateWith] = useState(curDate)
+  const [updateDateWith, setUpdateDateWith] = useState(new Date())
+  const [updatedDate, setUpdatedDate] = useState(updateDateWith)
   const [finalTextBoxContent, setFinalTextBoxContent] = useState("")
   const [activityName, setActivityName] = useState("")
-  /* const [updateActivityName, setUpdateActivityName] = useState("")
-  const [activityUnit, setActivityUnit] = useState("")
-  const [updateActivityUnit, setUpdateActivityUnit] = useState("") */
 
-  const addDateIntoTable = async () => { await addDate(curDate) }
-  const updateWaterValue = async () => { await updateWater(selectedActivityText, curDate) }
-  const updateSleepValue = async () => { await updateSleepTotal(selectedActivityText, curDate) }
+  useEffect(() => { updateDateWith.setHours(updateDateWith.getHours() - 5) }, [])
+
+  const addDateIntoTable = async () => { await addDate(updateCurDateWith) }
+  const updateWaterValue = async () => { await updateWater(selectedActivityText, updateCurDateWith) }
+  const updateSleepValue = async () => { await updateSleepTotal(selectedActivityText, updateCurDateWith) }
   const getBreakfastValue = async () => {
-    const breakfast = await getBreakfast(curDate)
+    const breakfast = await getBreakfast(updatedDate.toISOString().split("T")[0])
     breakfast[0].breakfastMeal !== null ? setSelectedActivityText(breakfast[0].breakfastMeal) : setSelectedActivityText("0")
   }
   const getLunchValue = async () => {
-    const lunch = await getLunch(curDate)
+    const lunch = await getLunch(updatedDate.toISOString().split("T")[0])
     lunch[0].lunchMeal !== null ? setSelectedActivityText(lunch[0].lunchMeal) : setSelectedActivityText("0")
   }
   const getDinnerValue = async () => {
-    const dinner = await getDinner(curDate)
+    const dinner = await getDinner(updatedDate.toISOString().split("T")[0])
     dinner[0].dinnerMeal !== null ? setSelectedActivityText(dinner[0].dinnerMeal) : setSelectedActivityText("0")
   }
   const getSnackValue = async () => {
-    const snack = await getSnacks(curDate)
+    const snack = await getSnacks(updatedDate.toISOString().split("T")[0])
     snack[0].snackMeal !== null ? setSelectedActivityText(snack[0].snackMeal) : setSelectedActivityText("0")
   }
   const getWaterValue = async () => {
-    const water = await getWater(curDate)
+    const water = await getWater(updatedDate.toISOString().split("T")[0])
     water[0].waterTotal !== null ? setSelectedActivityText(String(water[0].waterTotal)) : setSelectedActivityText("0")
   }
   const getSleepValue = async () => {
-    const sleep = await getSleepTotal(curDate)
+    const sleep = await getSleepTotal(updatedDate.toISOString().split("T")[0])
     sleep[0].sleepTotal !== null ? setSelectedActivityText(String(sleep[0].sleepTotal)) : setSelectedActivityText("0")
   }
   const getUnit = async (activityName: string) => {
@@ -72,7 +75,7 @@ const CheckIn = () => {
       setSelectedActivityUnit(unitResult?.exerciseUnit || "");
 
       // Get amount with type safety
-      const amount = await getActivityTypeAmnt(activityName, curDate);
+      const amount = await getActivityTypeAmnt(activityName, updatedDate.toISOString().split("T")[0]);
       setTextInputVal(amount.toString() || "0");
       if(unitResult != null) { setSelectedActivityUnit(unitResult.exerciseUnit); }
       else { setSelectedActivityUnit(""); }
@@ -88,13 +91,14 @@ const CheckIn = () => {
     
     if (!exists && activityName.trim() !== '') {
       try {
-        await addActivity(curDate, activityName, activityUnit);
+        await addActivity(updatedDate.toISOString().split("T")[0], activityName, activityUnit);
         setListOfActivityButtons(prev => [...prev, activityName]);
         setTextInputVal('');
       } catch (error) {
         console.error('Error adding activity:', error);
       }
     }
+    setActivityModalVisible(false)
   };
 
   const getPrevActivities = async () => {
@@ -113,21 +117,21 @@ const CheckIn = () => {
 
   const getMoodValue = async (timeOfDay: string) => {
     if (timeOfDay == "Morning Mood") {
-      const morningVal = await getMorningMood(curDate)
+      const morningVal = await getMorningMood(updatedDate.toISOString().split("T")[0])
 
       // Ensures the mood doesn't see "null" in the text input box when there is no value for the given date
       if (morningVal[0].morningMood !== null) { setSelectedActivityVal(morningVal[0].morningMood) }
       else { setSelectedActivityVal(0) }
     }
     else if (timeOfDay == "Midday Mood") {
-      const middayVal = await getMiddayMood(curDate)
+      const middayVal = await getMiddayMood(updatedDate.toISOString().split("T")[0])
 
       // Ensures the mood doesn't see "null" in the text input box when there is no value for the given date
       if (middayVal[0].middayMood !== null) { setSelectedActivityVal(middayVal[0].middayMood) }
       else { setSelectedActivityVal(0) }
     }
     else {
-      const nighttimeVal = await getNighttimeMood(curDate)
+      const nighttimeVal = await getNighttimeMood(updatedDate.toISOString().split("T")[0])
 
       // Ensures the mood doesn't see "null" in the text input box when there is no value for the given date
       if (nighttimeVal[0].nighttimeMood !== null) { setSelectedActivityVal(nighttimeVal[0].nighttimeMood) }
@@ -195,20 +199,21 @@ const CheckIn = () => {
 
   const handleSave = async (value: string) => {
     try {
+      addDate(updatedDate.toISOString().split("T")[0])
       if (selectedActivity === "Water") {
-        await updateWater(value, curDate);
+        await updateWater(value, updateCurDateWith);
         await getWaterValue();
       }
       else if (selectedActivity.includes("Mood")) {
-        await updateMood(value, curDate, selectedActivity);
+        await updateMood(value, updateCurDateWith, selectedActivity);
         await getMoodValue(selectedActivity);
       }
       else if (selectedActivity === "Sleep Total") {
-        await updateSleepTotal(value, curDate);
+        await updateSleepTotal(value, updateCurDateWith);
         await getSleepValue();
       }
       else if (["Breakfast", "Lunch", "Dinner", "Snacks"].includes(selectedActivity)) {
-        await updateFood(value, curDate, selectedActivity);
+        await updateFood(value, updateCurDateWith, selectedActivity);
         // Refresh the appropriate meal value
         switch (selectedActivity) {
           case "Breakfast": await getBreakfastValue(); break;
@@ -219,7 +224,7 @@ const CheckIn = () => {
       }
       else {
         // Handle custom activities
-        await updateActivity(curDate, selectedActivity, Number(value));
+        await updateActivity(updateCurDateWith, selectedActivity, Number(value));
         await getUnit(selectedActivity);
       }
       setActivityModalVisible(false);
@@ -231,6 +236,7 @@ const CheckIn = () => {
 
   const handleCreateSave = (name: string, unit: string) => {
     if (name.trim() && unit.trim()) {
+      addDate(updatedDate.toISOString().split("T")[0])
       addActivityButton(name, unit);
       setActivityModalVisible(false);
     }
@@ -261,14 +267,41 @@ const CheckIn = () => {
     if (selectedActivity == "Water") updateWaterValue()
     else if (selectedActivity == "Sleep Total") updateSleepValue()
   }, [selectedActivityText])
+  
+  
+  useEffect(() => { setUpdatedDate(updateDateWith) }, [updateDateWith])
+  useEffect(() => { setUpdateCurDateWith(updatedDate.toISOString()) }, [updatedDate])
+  useEffect(() => { setCurDate(updateCurDateWith) }, [updateCurDateWith])
 
   return (
     <>
       <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         <SafeAreaView style={styles.container}>
           <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : undefined} style={{ flex: 1 }}>
-            <Text style={styles.question}>[Example Question to Go Here]</Text>
-            
+            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+              <TouchableOpacity style={styles.arrowButtonsLocation} onPress={() => {
+                currentDay = new Date(updateDateWith.getFullYear(), updateDateWith.getMonth(), updateDateWith.getDate() - 1)
+                currentDay.setHours(updateDateWith.getHours())
+                setUpdateDateWith(currentDay)
+              }}>
+                <Image style={styles.arrowButtonsImgs} source={require('../assets/images/leftArrow.png')} />
+              </TouchableOpacity>
+              
+              <TouchableWithoutFeedback onPress={() => {
+                currentDay = new Date();
+                currentDay = new Date(currentDay.getFullYear(), currentDay.getMonth(), currentDay.getDate())
+                setUpdateDateWith(currentDay);
+              }}>
+                <Text style={{ fontSize: 38, textAlign: 'center', width: 280 }}>{getMonthName(updateDateWith.getMonth())} {updateDateWith.getDate()}, {updateDateWith.getFullYear()}</Text>
+              </TouchableWithoutFeedback>
+              <TouchableOpacity style={styles.arrowButtonsLocation} onPress={() => {
+                currentDay = new Date(updatedDate.getFullYear(), updatedDate.getMonth(), updatedDate.getDate() + 1)
+                setUpdateDateWith(currentDay);
+              }}>
+                <Image style={styles.arrowButtonsImgs} source={require('../assets/images/rightArrow.png')} />
+              </TouchableOpacity>
+            </View>
+
             <View style={{ backgroundColor: 'azure', flex: 5 }}>
               <View style={styles.activitiesTitleHeader}>
                 <Text style={styles.activitiesTitleText}>Activities</Text>
@@ -368,12 +401,12 @@ const CheckIn = () => {
               ? 
                 activityName.includes("Mood") 
                 ? 
-                  <MoodGrid curDate={curDate} selectedActivityName={activityName} selectedActivityUnit={selectedActivityUnit} selectedActivityText={String(selectedActivityVal)} setSelectedActivityText={setSelectedActivityText} onSave={handleSave} />
+                  <MoodGrid curDate={updatedDate} selectedActivityName={activityName} selectedActivityUnit={selectedActivityUnit} selectedActivityText={String(selectedActivityVal)} setSelectedActivityText={setSelectedActivityText} onSave={handleSave} />
                 : 
                   <>
                     {/* Activity Unit Input Box & Save Button */}
                     <UpdateModal
-                      curDate={curDate}
+                      curDate={updatedDate}
                       selectedActivityName={activityName}
                       selectedActivityUnit={selectedActivityUnit}
                       selectedActivityText={String(textInputVal)}
