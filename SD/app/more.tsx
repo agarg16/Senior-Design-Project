37ebb99d3506e496c28a85c47fdfe515cde9ae5b
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-12 07:28:35 -0500

    Update more.tsx
    - General update to re-format the buttons and create modals upon selecting one of the buttons

--- SD/app/more.tsx
+++ SD/app/more.tsx
@@ -1,87 +1,69 @@
-import React from 'react'
-import { Text, StyleSheet, SafeAreaView, FlatList, Dimensions, Platform, TouchableOpacity } from "react-native"
-
-var width = 0
-var height = 0
-
-if(Platform.OS === 'web') {
-  width  = Dimensions.get('window').width
-  height = Dimensions.get('window').height
-}
-else {
-  width  = Dimensions.get('screen').width
-  height = Dimensions.get('screen').height
-}
-const boxSize = width <= height ? (width * 0.32) : (height * 0.32)
+import React, { useState, useEffect } from 'react'
+import { Text, View, SafeAreaView, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform } from "react-native"
+import { styles, modalStyles } from '../styles/MoreStyles'
+import { Ionicons } from '@expo/vector-icons'
 
 const DATA = [
-  { title: 'Journal' },
-  { title: 'Practice' },
-  { title: 'Learn More' },
-  { title: 'Helpful Resources' }
+  { title: 'Self-Assessment' },
+  { title: 'Graphs' },
+  { title: 'Learn More' }
 ]
 
-type ItemProps = {title: string}
-
-const Item = ({title}: ItemProps) => (
-  <TouchableOpacity style={styles.infoItemBox}>
-    <Text style={styles.infoItemText}>{title}</Text>
-  </TouchableOpacity>
-)
-
 const More = () => {
+  const [viewModalVisible, setViewModalVisible] = useState(false)
+  const [modalType, setModalType] = useState("")
+
+  type ItemProps = {title: string}
+  const Item = ({title}: ItemProps) => {
+    return (
+      <TouchableOpacity style={styles.infoItemBox} onPress={() => {
+        setViewModalVisible(true)
+        setModalType(title)
+      }}>
+        <Text style={styles.infoItemText}>{title}</Text>
+      </TouchableOpacity>
+  )}
+
   return (
-    
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Extra Information</Text>
-      
-      {/* Displays the boxes of info */}
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item title={item.title} />}
-        scrollEnabled={false}
-        numColumns={2}
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={{ borderColor: 'black', borderBottomWidth: 1, justifyContent: 'center', flex: 1}}>
+        <Text style={styles.title}>Extra Information</Text>
+        </View>
         
-        contentContainerStyle={styles.moreInfoSection}
-      />
-    </SafeAreaView>
+        <View style={styles.moreInfoSection}>
+        {/* Displays the boxes of info */}
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <Item title={item.title} />}
+          scrollEnabled={false}
+          numColumns={1}
+          
+          contentContainerStyle={styles.moreInfoSection}
+        />
+        </View>
+      </SafeAreaView>
+
+      {/* Modals */}
+        <Modal visible={viewModalVisible} onRequestClose={() => setViewModalVisible(false)} animationType='slide' presentationStyle='pageSheet'>
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : undefined} style={{ backgroundColor: 'white', flex: 1 }}>
+              <SafeAreaView style={modalStyles.container}>
+                  {/* Back Button */}
+                  <TouchableOpacity style={modalStyles.backButton} onPress={() => setViewModalVisible(false)}>
+                    <Ionicons name="chevron-back" color={'#18576D'} size={20} />
+                    <Text style={{ color: '#18576D', fontSize: 16 }}>Back</Text>
+                  </TouchableOpacity>
+
+                  <Text style={{fontSize: 32, textAlign: 'center', paddingBottom: 32}}>{modalType}</Text>
+              </SafeAreaView>
+            </KeyboardAvoidingView>
+          </TouchableWithoutFeedback>
+        </Modal>
+    </>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    flex: 1
-  },
-  title: {
-    fontSize: 44,
-    marginTop: 12,
-    marginBottom: 16,
-    textAlign: 'center',
-  },
-  moreInfoSection: {
-    backgroundColor: 'azure',
-    borderColor: 'black',
-    borderTopWidth: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1
-  },
-  infoItemBox: {
-    backgroundColor: 'lightblue',
-    borderColor: '#6c8790',
-    borderWidth: 2,
-    borderRadius: 20,
-    width: boxSize,
-    height: boxSize,
-    margin: (boxSize * 0.13),
-    justifyContent: 'center'
-  },
-  infoItemText: {
-    fontSize: (boxSize * 0.18),
-    textAlign: 'center'
-  }
-})
+
 
 export default More
