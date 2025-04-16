6c1d2789fb17f889a5e872a4fc24ab5f6ab78797
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-15 22:14:13 -0500

    Update ActivityButton.tsx
    - Removed unused Image import

--- SD/components/CheckInComponents/ActivityButton.tsx
+++ SD/components/CheckInComponents/ActivityButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Text, Image } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 import { styles } from '../../styles/CheckInStyles';
 
 export const ActivityButton = ({ 
