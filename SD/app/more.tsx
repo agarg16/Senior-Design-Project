86ec5fac2f40b7994dd879e8ea12bd7e5fd46648
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-15 22:13:21 -0500

    Update more.tsx
    - Removed "Learn More" button

--- SD/app/more.tsx
+++ SD/app/more.tsx
@@ -6,7 +6,6 @@ import { ModalUsed } from '../components/MoreComponents/MoreModal'
 const DATA = [
   { title: 'Self-Assessment' },
   { title: 'Graphs' },
-  { title: 'Learn More' },
   { title: 'Licenses' }
 ]
 
@@ -15,6 +14,7 @@ const More = () => {
   const [modalType, setModalType] = useState("")
   const [updateType, setUpdateType] = useState(false) /* Provides the user with the ability to re-open the most recently closed module (upon updating this value) */
 
+  /* Lists the buttons the user can select on the (main) Extra Information screen */
   type ItemProps = {title: string}
   const Item = ({title}: ItemProps) => {
     return (
@@ -31,22 +31,18 @@ const More = () => {
 
   /* Ensures the user can interact with the different buttons more than once */
   const Modals = () => {
-    return (
-      <View>
-        {ModalUsed(modalType, viewModalVisible)}
-      </View>
-    )
+    return ( <View>{ModalUsed(modalType, viewModalVisible)}</View> )
   }
 
   return (
-    <>
-      <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container}>
+        {/* Extra Information Title Header */}
         <View style={{ borderColor: 'black', borderBottomWidth: 1, justifyContent: 'center', flex: 1}}>
-        <Text style={styles.title}>Extra Information</Text>
+          <Text style={styles.title}>Extra Information</Text>
         </View>
         
+        {/* Displays the Extra Information's Boxes of Info */}
         <View style={styles.moreInfoSection}>
-          {/* Displays the Extra Information's boxes of info */}
           <FlatList
             data={DATA}
             renderItem={({item}) => <Item title={item.title} />}
@@ -56,8 +52,7 @@ const More = () => {
             contentContainerStyle={styles.moreInfoSection}
           />
         </View>
-      </SafeAreaView>      
-    </>
+    </SafeAreaView>
   )
 }
 
