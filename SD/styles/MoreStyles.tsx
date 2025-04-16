0e68bbfa2fbb3a38439b3064281b390e867ae155
agarg16 <166842753+agarg16@users.noreply.github.com>
2025-04-15 22:28:31 -0500

    Update MoreStyles.tsx
    - Added styles for the graphs and dropdown menu

--- SD/styles/MoreStyles.tsx
+++ SD/styles/MoreStyles.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native'
 
-const { width, height } = Dimensions.get('screen'); // Width and height of the screen
+const { width, height } = Dimensions.get('screen') // Width and height of the screen
 
 // Height of the boxes holding the days of the current month
 const boxSize = width <= height ? (width * 0.32) : (height * 0.32)
@@ -37,7 +37,7 @@ export const styles = StyleSheet.create({
       fontSize: (boxSize * 0.18),
       textAlign: 'center'
     }
-});
+})
 
 // StyleSheet for All Modals
 export const modalStyles = StyleSheet.create({
@@ -53,13 +53,96 @@ export const modalStyles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
   },
+  modalTypeTitle: {
+    backgroundColor: 'white',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    fontSize: 32,
+    textAlign: 'center',
+    padding: 32,
+    margin: 0.25
+  },
+  graphInputBox: {
+    backgroundColor: 'white',
+    borderWidth: 2,
+    borderRadius: 15,
+    padding: 8,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 8,
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+  graphDropdownButton: {
+    borderWidth: 1,
+    borderRadius: 4,
+    width: 165,
+    marginLeft: 40,
+    paddingTop: 8,
+    paddingBottom: 8,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  graphDropdownMenu: {
+    backgroundColor: 'white',
+    borderLeftWidth: 0.5,
+    borderRightWidth: 0.5,
+    borderBottomWidth: 0.5,
+    width: 165,
+    marginLeft: 40,
+    zIndex: 1
+  },
+  graphDropdownMenuButton: {
+    borderLeftWidth: 0.5,
+    borderRightWidth: 0.5,
+    width: 165,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute'
+  },
+  graphInputEnterButton: {
+    backgroundColor: '#18576D',
+    borderColor: '#43B0D8',
+    borderWidth: 0.5,
+    borderRadius: 20,
+    width: 75,
+    height: 30,
+    justifyContent: 'center',
+    marginRight: 20
+  },
+  graphBox: {
+    backgroundColor: 'white',
+    borderColor: 'gray',
+    borderWidth: 1.25,
+    borderRadius: 20,
+    width: 350,
+    height: 315,
+    padding: 8,
+    alignSelf: 'center'
+  },
+  yAxisImages: {
+    width: 20,
+    height: 20,
+    margin: 3.5,
+    marginLeft: 12,
+    marginBottom: 6.5
+  },
+  packageButtons: {
+    backgroundColor: 'lightblue',
+    borderColor: 'gray',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    padding: 16,
+    marginBottom: 4
+  },
   licenseContainer: {
     backgroundColor: 'lightblue',
     borderBottomWidth: 1,
-    height: '70%'
+    height: '65%'
   },
   licenseText: {
     padding: 8,
     fontSize: 16
   }
-});
+})
